162137ff0b0daf63393e865c99763b14d91351a5
NeyCoto <86335774+NeyCoto@users.noreply.github.com>
2022-10-13 22:19:33 -0500

    Proyecto base

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="contenedor">
+  <div class="img1"></div><div class="img2"></div><div class="img3"></div><div class="img4"></div><div class="img5"></div><div class="img6"></div>
+  <center>
+    <div class="center"></div>
+  </center>
+</div>
+</template>
+
+<style scoped>
+.img1{
+  width: 100px;
+  height: 100px;
+  float: left;
+}
+.img2{
+  width: 100px;
+  height: 100px;
+  float: left;
+}
+.img3{
+  width: 100px;
+  height: 100px;
+  float: left;
+}
+.img4{
+  width: 100px;
+  height: 100px;
+  float: left;
+}
+.img5{
+  width: 100px;
+  height: 100px;
+  float: left;
+}
+.img6{
+  width: 100px;
+  height: 100px;
+  float: left;
+}
+.contenedor{
+  background-color: aqua;
+  width: 600px;
+  height: 500px;
+}
+.center{
+  height: 300px;
+  width: 400px;
+  background-color: blue;
+}
+</style>
